62ba476ac58271ea2dde8cc3bacc1e458af0279d
wcj343169893@163.com <wcj343169893@163.com@afbb26fe-e4aa-11de-b41e-e93f42a66952>
2009-12-14 16:14:28 +0000

    完成business的entity关系映射和dao基本方法

--- Buildingsale/src/com/zuxia/buildingsale/business/entity/DataDict.java
+++ Buildingsale/src/com/zuxia/buildingsale/business/entity/DataDict.java
@@ -1,7 +1,10 @@
 package com.zuxia.buildingsale.business.entity;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.zuxia.buildingsale.house.entity.HouseInfo;
+import com.zuxia.buildingsale.sys.entity.Employee;
 
 /**
  * DataDict entity. @author MyEclipse Persistence Tools
@@ -15,130 +18,251 @@ public class DataDict implements java.io.Serializable {
 	private DataDictType dataDictType;
 	private String ddValue;
 	private String ddRemark;
-	private Set houseInfos = new HashSet(0);
-	private Set loftInfosForLoftBuildType = new HashSet(0);
-	private Set clientInfos = new HashSet(0);
-	private Set dealInfosForDealState = new HashSet(0);
-	private Set employees = new HashSet(0);
-	private Set loftInfosForLoftComtyType = new HashSet(0);
-	private Set dealInfosForDealPayStyle = new HashSet(0);
+	/**
+	 * houseInfos属性概述 房屋信息
+	 * 
+	 */
+	private List<HouseInfo> houseInfos = new ArrayList<HouseInfo>();
+	/**
+	 * clientInfos属性概述 客户信息
+	 * 
+	 */
+	private List<ClientInfo> clientInfos = new ArrayList<ClientInfo>();
+	/**
+	 * employees属性概述 员工
+	 * 
+	 */
+	private List<Employee> employees = new ArrayList<Employee>();
+	/**
+	 * loftInfosForLoftBuildType属性概述 楼盘建筑类型
+	 * 
+	 */
+	private List<DataDict> loftInfosForLoftBuildType = new ArrayList<DataDict>();
+	/**
+	 * loftInfosForLoftComtyType属性概述 楼盘物业类型
+	 * 
+	 */
+	private List<DataDict> loftInfosForLoftComtyType = new ArrayList<DataDict>();
+	/**
+	 * dealInfosForDealState属性概述 交易表的状态
+	 * 
+	 */
+	private List<DataDict> dealInfosForDealState = new ArrayList<DataDict>();
+	/**
+	 * dealInfosForDealPayStyle属性概述 交易表的付款方式
+	 * 
+	 */
+	private List<DataDict> dealInfosForDealPayStyle = new ArrayList<DataDict>();
 
-	// Constructors
-
-	/** default constructor */
-	public DataDict() {
-	}
-
-	/** minimal constructor */
-	public DataDict(DataDictType dataDictType, String ddValue) {
-		this.dataDictType = dataDictType;
-		this.ddValue = ddValue;
-	}
-
-	/** full constructor */
-	public DataDict(DataDictType dataDictType, String ddValue, String ddRemark,
-			Set houseInfos, Set loftInfosForLoftBuildType, Set clientInfos,
-			Set dealInfosForDealState, Set employees,
-			Set loftInfosForLoftComtyType, Set dealInfosForDealPayStyle) {
-		this.dataDictType = dataDictType;
-		this.ddValue = ddValue;
-		this.ddRemark = ddRemark;
-		this.houseInfos = houseInfos;
-		this.loftInfosForLoftBuildType = loftInfosForLoftBuildType;
-		this.clientInfos = clientInfos;
-		this.dealInfosForDealState = dealInfosForDealState;
-		this.employees = employees;
-		this.loftInfosForLoftComtyType = loftInfosForLoftComtyType;
-		this.dealInfosForDealPayStyle = dealInfosForDealPayStyle;
-	}
-
-	// Property accessors
-
+	/**
+	 * ddId属性的get方法
+	 * 
+	 * @return the ddId
+	 */
 	public Integer getDdId() {
-		return this.ddId;
+		return ddId;
 	}
 
+	/**
+	 * ddId属性的set方法
+	 * 
+	 * @param ddId
+	 *            the ddId to set
+	 */
 	public void setDdId(Integer ddId) {
 		this.ddId = ddId;
 	}
 
+	/**
+	 * dataDictType属性的get方法
+	 * 
+	 * @return the dataDictType
+	 */
 	public DataDictType getDataDictType() {
-		return this.dataDictType;
+		return dataDictType;
 	}
 
+	/**
+	 * dataDictType属性的set方法
+	 * 
+	 * @param dataDictType
+	 *            the dataDictType to set
+	 */
 	public void setDataDictType(DataDictType dataDictType) {
 		this.dataDictType = dataDictType;
 	}
 
+	/**
+	 * ddValue属性的get方法
+	 * 
+	 * @return the ddValue
+	 */
 	public String getDdValue() {
-		return this.ddValue;
+		return ddValue;
 	}
 
+	/**
+	 * ddValue属性的set方法
+	 * 
+	 * @param ddValue
+	 *            the ddValue to set
+	 */
 	public void setDdValue(String ddValue) {
 		this.ddValue = ddValue;
 	}
 
+	/**
+	 * ddRemark属性的get方法
+	 * 
+	 * @return the ddRemark
+	 */
 	public String getDdRemark() {
-		return this.ddRemark;
+		return ddRemark;
 	}
 
+	/**
+	 * ddRemark属性的set方法
+	 * 
+	 * @param ddRemark
+	 *            the ddRemark to set
+	 */
 	public void setDdRemark(String ddRemark) {
 		this.ddRemark = ddRemark;
 	}
 
-	public Set getHouseInfos() {
-		return this.houseInfos;
+	/**
+	 * houseInfos属性的get方法
+	 * 
+	 * @return the houseInfos
+	 */
+	public List<HouseInfo> getHouseInfos() {
+		return houseInfos;
 	}
 
-	public void setHouseInfos(Set houseInfos) {
+	/**
+	 * houseInfos属性的set方法
+	 * 
+	 * @param houseInfos
+	 *            the houseInfos to set
+	 */
+	public void setHouseInfos(List<HouseInfo> houseInfos) {
 		this.houseInfos = houseInfos;
 	}
 
-	public Set getLoftInfosForLoftBuildType() {
-		return this.loftInfosForLoftBuildType;
+	/**
+	 * clientInfos属性的get方法
+	 * 
+	 * @return the clientInfos
+	 */
+	public List<ClientInfo> getClientInfos() {
+		return clientInfos;
 	}
 
-	public void setLoftInfosForLoftBuildType(Set loftInfosForLoftBuildType) {
-		this.loftInfosForLoftBuildType = loftInfosForLoftBuildType;
-	}
-
-	public Set getClientInfos() {
-		return this.clientInfos;
-	}
-
-	public void setClientInfos(Set clientInfos) {
+	/**
+	 * clientInfos属性的set方法
+	 * 
+	 * @param clientInfos
+	 *            the clientInfos to set
+	 */
+	public void setClientInfos(List<ClientInfo> clientInfos) {
 		this.clientInfos = clientInfos;
 	}
 
-	public Set getDealInfosForDealState() {
-		return this.dealInfosForDealState;
+	/**
+	 * employees属性的get方法
+	 * 
+	 * @return the employees
+	 */
+	public List<Employee> getEmployees() {
+		return employees;
 	}
 
-	public void setDealInfosForDealState(Set dealInfosForDealState) {
-		this.dealInfosForDealState = dealInfosForDealState;
-	}
-
-	public Set getEmployees() {
-		return this.employees;
-	}
-
-	public void setEmployees(Set employees) {
+	/**
+	 * employees属性的set方法
+	 * 
+	 * @param employees
+	 *            the employees to set
+	 */
+	public void setEmployees(List<Employee> employees) {
 		this.employees = employees;
 	}
 
-	public Set getLoftInfosForLoftComtyType() {
-		return this.loftInfosForLoftComtyType;
+	/**
+	 * loftInfosForLoftBuildType属性的get方法
+	 * 
+	 * @return the loftInfosForLoftBuildType
+	 */
+	public List<DataDict> getLoftInfosForLoftBuildType() {
+		return loftInfosForLoftBuildType;
 	}
 
-	public void setLoftInfosForLoftComtyType(Set loftInfosForLoftComtyType) {
+	/**
+	 * loftInfosForLoftBuildType属性的set方法
+	 * 
+	 * @param loftInfosForLoftBuildType
+	 *            the loftInfosForLoftBuildType to set
+	 */
+	public void setLoftInfosForLoftBuildType(
+			List<DataDict> loftInfosForLoftBuildType) {
+		this.loftInfosForLoftBuildType = loftInfosForLoftBuildType;
+	}
+
+	/**
+	 * loftInfosForLoftComtyType属性的get方法
+	 * 
+	 * @return the loftInfosForLoftComtyType
+	 */
+	public List<DataDict> getLoftInfosForLoftComtyType() {
+		return loftInfosForLoftComtyType;
+	}
+
+	/**
+	 * loftInfosForLoftComtyType属性的set方法
+	 * 
+	 * @param loftInfosForLoftComtyType
+	 *            the loftInfosForLoftComtyType to set
+	 */
+	public void setLoftInfosForLoftComtyType(
+			List<DataDict> loftInfosForLoftComtyType) {
 		this.loftInfosForLoftComtyType = loftInfosForLoftComtyType;
 	}
 
-	public Set getDealInfosForDealPayStyle() {
-		return this.dealInfosForDealPayStyle;
+	/**
+	 * dealInfosForDealState属性的get方法
+	 * 
+	 * @return the dealInfosForDealState
+	 */
+	public List<DataDict> getDealInfosForDealState() {
+		return dealInfosForDealState;
 	}
 
-	public void setDealInfosForDealPayStyle(Set dealInfosForDealPayStyle) {
+	/**
+	 * dealInfosForDealState属性的set方法
+	 * 
+	 * @param dealInfosForDealState
+	 *            the dealInfosForDealState to set
+	 */
+	public void setDealInfosForDealState(List<DataDict> dealInfosForDealState) {
+		this.dealInfosForDealState = dealInfosForDealState;
+	}
+
+	/**
+	 * dealInfosForDealPayStyle属性的get方法
+	 * 
+	 * @return the dealInfosForDealPayStyle
+	 */
+	public List<DataDict> getDealInfosForDealPayStyle() {
+		return dealInfosForDealPayStyle;
+	}
+
+	/**
+	 * dealInfosForDealPayStyle属性的set方法
+	 * 
+	 * @param dealInfosForDealPayStyle
+	 *            the dealInfosForDealPayStyle to set
+	 */
+	public void setDealInfosForDealPayStyle(
+			List<DataDict> dealInfosForDealPayStyle) {
 		this.dealInfosForDealPayStyle = dealInfosForDealPayStyle;
 	}
 
